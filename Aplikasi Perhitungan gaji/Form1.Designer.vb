9be8bc3b792f073b06d96769163054e96e8a369f
Moon-Young <Moon-Young@DESKTOP-7S6QME0>
2024-02-18 14:56:44 +0700

    Add project files.

---
+++ Aplikasi Perhitungan gaji/Form1.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TambahStaffToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExportToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.KeluarToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 118)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nama Staff"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 144)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Jabatan"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 170)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Gaji Pokok"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 196)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Tunjangan"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 222)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(37, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Bonus"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 248)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(42, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Lembur"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 274)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(53, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Potongan"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 300)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(52, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Total Gaji"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(120, 141)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(192, 20)
+        Me.TextBox2.TabIndex = 10
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(120, 167)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(192, 20)
+        Me.TextBox3.TabIndex = 11
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(120, 193)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(192, 20)
+        Me.TextBox4.TabIndex = 12
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(120, 219)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(192, 20)
+        Me.TextBox5.TabIndex = 13
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(120, 245)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(192, 20)
+        Me.TextBox6.TabIndex = 14
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(120, 271)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(192, 20)
+        Me.TextBox7.TabIndex = 15
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(120, 297)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(192, 20)
+        Me.TextBox8.TabIndex = 16
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(120, 335)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(112, 23)
+        Me.Button2.TabIndex = 18
+        Me.Button2.Text = "Hitung Ulang Gaji"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Tai Le", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(38, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(262, 27)
+        Me.Label1.TabIndex = 20
+        Me.Label1.Text = "Aplikasi Perhitungan Gaji"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(120, 88)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(192, 21)
+        Me.ComboBox1.TabIndex = 21
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(120, 115)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(192, 20)
+        Me.TextBox1.TabIndex = 9
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 91)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(25, 13)
+        Me.Label10.TabIndex = 22
+        Me.Label10.Text = "NIK"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(344, 24)
+        Me.MenuStrip1.TabIndex = 24
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TambahStaffToolStripMenuItem, Me.ExportToolStripMenuItem, Me.KeluarToolStripMenuItem1})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'TambahStaffToolStripMenuItem
+        '
+        Me.TambahStaffToolStripMenuItem.Name = "TambahStaffToolStripMenuItem"
+        Me.TambahStaffToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.TambahStaffToolStripMenuItem.Text = "Tambah Staff"
+        '
+        'ExportToolStripMenuItem
+        '
+        Me.ExportToolStripMenuItem.Name = "ExportToolStripMenuItem"
+        Me.ExportToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExportToolStripMenuItem.Text = "Export to excel"
+        '
+        'KeluarToolStripMenuItem1
+        '
+        Me.KeluarToolStripMenuItem1.Name = "KeluarToolStripMenuItem1"
+        Me.KeluarToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.KeluarToolStripMenuItem1.Text = "Keluar"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 387)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Perhitungan Gaji"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TambahStaffToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExportToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents KeluarToolStripMenuItem1 As ToolStripMenuItem
+End Class
---
+++ Aplikasi Perhitungan gaji/Form1.vb
@@ -0,0 +1,183 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class Form1
+
+    ' Deklarasi variabel Excel
+    Private excelApp As Excel.Application
+    Private excelWorkbook As Excel.Workbook
+    Private excelWorksheet As Excel.Worksheet
+
+    'Fungsi untuk mengekspor data ke Excel
+    Private Sub ExportToExcel()
+        ' Inisialisasi Excel
+        excelApp = New Excel.Application
+        excelWorkbook = excelApp.Workbooks.Add()
+        excelWorksheet = excelWorkbook.Sheets("Sheet1")
+
+        ' Menulis data dari TextBox ke Excel
+        excelWorksheet.Cells(1, 1).Value = "Nama"
+        excelWorksheet.Cells(1, 2).Value = "Pekerjaan"
+        excelWorksheet.Cells(1, 3).Value = "Gaji Pokok"
+        excelWorksheet.Cells(1, 4).Value = "Tunjangan"
+        excelWorksheet.Cells(1, 5).Value = "Bonus"
+        excelWorksheet.Cells(1, 6).Value = "Lembur"
+        excelWorksheet.Cells(1, 7).Value = "Potongan"
+        excelWorksheet.Cells(1, 8).Value = "Total Gaji"
+
+        ' Menulis data dari TextBox ke Excel
+        excelWorksheet.Cells(2, 1).Value = TextBox1.Text
+        excelWorksheet.Cells(2, 2).Value = TextBox2.Text
+        excelWorksheet.Cells(2, 3).Value = TextBox3.Text
+        excelWorksheet.Cells(2, 4).Value = TextBox4.Text
+        excelWorksheet.Cells(2, 5).Value = TextBox5.Text
+        excelWorksheet.Cells(2, 6).Value = TextBox6.Text
+        excelWorksheet.Cells(2, 7).Value = TextBox7.Text
+        excelWorksheet.Cells(2, 8).Value = TextBox8.Text
+
+        ' Menyimpan Excel
+        Dim saveFileDialog1 As New SaveFileDialog()
+        saveFileDialog1.Filter = "Excel Files|*.xlsx"
+        saveFileDialog1.Title = "Save an Excel File"
+        saveFileDialog1.ShowDialog()
+
+        If saveFileDialog1.FileName <> "" Then
+            excelWorkbook.SaveAs(saveFileDialog1.FileName)
+            excelWorkbook.Close()
+            excelApp.Quit()
+        End If
+    End Sub
+
+    ' Store Data
+    Private dataMapping As New Dictionary(Of String, String) From {
+        {"880001", "Raihan, IT, Rp.6.000.000"},
+        {"880002", "Rahma, Akuntan, Rp.6.000.000"},
+        {"880003", "Bagas, General Affair, Rp.6.200.000"},
+        {"880004", "Yudi, Administrasi, Rp.5.400.000"},
+        {"880005", "Ilham, Human Resources, Rp.6.200.000"}
+    }
+
+    ' Menampilkan dataMapping pada ComboBox1
+    ' Membuat TextBox1,2,3 menjadi read only
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.AddRange(dataMapping.Keys.ToArray())
+
+        TextBox1.ReadOnly = True
+        TextBox2.ReadOnly = True
+        TextBox3.ReadOnly = True
+    End Sub
+
+    ' Deklarasi TextBox
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+        UpdateGajiTotal()
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+        UpdateGajiTotal()
+    End Sub
+
+    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+        UpdateGajiTotal()
+    End Sub
+
+    Private Sub TextBox7_TextChanged(Sender As Object, e As EventArgs) Handles TextBox7.TextChanged
+        UpdateGajiTotal()
+    End Sub
+
+    ' Fungsi perhitungan gaji
+    Private Sub UpdateGajiTotal()
+        ' Pastikan TextBox3 berisi angka sebelum melakukan pertambahan
+        Dim gajiPokok As Integer
+        If Integer.TryParse(TextBox3.Text.Replace("Rp.", "").Replace(".", ""), gajiPokok) Then
+            ' Pastikan TextBox4 berisi angka
+            Dim tambahan As Integer = If(Integer.TryParse(TextBox4.Text, tambahan), tambahan, 0)
+            Dim tambahan1 As Integer = If(Integer.TryParse(TextBox5.Text, tambahan1), tambahan1, 0)
+            Dim tambahan2 As Integer = If(Integer.TryParse(TextBox6.Text, tambahan2), tambahan2, 0)
+            Dim tambahan3 As Integer = If(Integer.TryParse(TextBox7.Text, tambahan3), tambahan3, 0)
+
+            ' Hitung total gaji dengan tambahan dari TextBox5, TextBox6, dan TextBox4
+            Dim totalGaji As Integer = gajiPokok + tambahan + tambahan1 + tambahan2 - tambahan3
+            TextBox8.Text = "Rp." & totalGaji.ToString("N0")
+
+        Else
+            TextBox8.Text = "Invalid Input"
+        End If
+    End Sub
+
+    ' Menampilkan dataMapping pada ComboBox1 jika diklik untuk mengisi otomatis TextBox1,2,3
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim selectedValue As String = ComboBox1.SelectedItem.ToString()
+
+        If dataMapping.ContainsKey(selectedValue) Then
+            Dim values() As String = dataMapping(selectedValue).Split(", ")
+            TextBox1.Text = values(0) ' Nama
+            TextBox2.Text = values(1) ' Pekerjaan
+            TextBox3.Text = values(2) ' Gaji Pokok
+        End If
+
+        TextBox1.ReadOnly = True
+        TextBox2.ReadOnly = True
+        TextBox3.ReadOnly = True
+    End Sub
+
+    ' Tombol reset TextBox untuk menghitung kembali
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Clear()
+        TextBox6.Clear()
+        TextBox7.Clear()
+        TextBox8.Clear()
+    End Sub
+
+    ' Deklarasi MenuBar
+    Private Sub MenuToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MenuToolStripMenuItem.Click
+
+    End Sub
+
+    ' Deklarasi pemanggilan fungi ExportToExcel pada sub menu Export to excel
+    Private Sub ExportToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExportToolStripMenuItem.Click
+        ExportToExcel()
+    End Sub
+
+    ' Deklarasi fungsi penambahan data staff baru
+    Private Sub TambahStaffToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TambahStaffToolStripMenuItem.Click
+        ' Meminta pengguna untuk memasukan NIK staff baru
+        Dim id As String = InputBox("Masukkan NIK staf baru:", "Tambah Staf")
+
+        ' Periksa apakah pengguna memasukkan NIK atau membatalkan
+        If Not String.IsNullOrEmpty(id) Then
+            ' Periksa apakah NIK sudah ada dalam dataMapping
+            If Not dataMapping.ContainsKey(id) Then
+                ' Jika NIK belum ada, minta pengguna untuk memasukkan detail staf
+                Dim detail As String = InputBox("Masukkan detail staf (Nama, Pekerjaan, Gaji):", "Tambah Staf")
+                ' Tambahkan data baru ke dataMapping
+                dataMapping.Add(id, detail)
+                ' Perbarui ComboBox1
+                ComboBox1.Items.Add(id)
+                MessageBox.Show("Staf berhasil ditambahkan.", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("NIK staf sudah ada.", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+        End If
+    End Sub
+
+    ' Deklarasi fungsi keluar aplikasi
+    Private Sub KeluarToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem1.Click
+        Me.Close()
+    End Sub
+
+End Class
